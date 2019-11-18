b104d7987d24634dbb76ead9ce06b4fb9ba8d5d1
MarthaRM <rdz.marthaelena@gmail.com>
2019-11-17 21:06:12 -0600

    correccion de headers

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers', 'x-requested-with, Content-Type, X-Token-Auth, Authorization');
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
